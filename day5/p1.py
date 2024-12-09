250cdf8d97288af3552ad4f65ccce612dd257dce
DenizYalim <116675869+DenizYalim@users.noreply.github.com>
2024-12-09 15:09:56 +0300

    Added a solution to p1, currently unoptimized for large input sizes though

--- day5/p1.py
+++ day5/p1.py
@@ -4,73 +4,90 @@ with open("input", "r") as file:
     updates = [text.split(',') for text in text[text.index(
         '') + 1:]]  # No need to turn string numbers in to ints, they will be just used as symbols
 
+print(ordering_rules)
+print(updates)
+
 
 class Node:
-    def __init__(self, node_id):
-        self.node_id = node_id
-        self.afters = []
+    def __init__(self, name):
+        self.name = name
+        self.directed_nodes = []
 
-    def addAfter(self, after):
-        self.afters.append(after)
+    def addNode(self, node: 'Node'):
+        self.directed_nodes.append(node)
 
-    def printAfters(self):
-        for after in self.afters:
-            print(after, end=' ')
-
-    def getAfters(self):
-        return self.afters
+    def getDirectedNodes(self):
+        return self.directed_nodes
 
 
 class Graph:
-    def __init__(self):
-        self.nodes = {}
+    def __init__(self, ordering_rules: list):
+        self.nodes = []
+        for node, node2 in ordering_rules:
+            self.defineNode(node)
+            self.defineNode(node2)
 
-    def addNode(self, node):
-        if node in self.nodes:
-            return
-        self.nodes[node] = Node(node)
+        for tuple in ordering_rules:
+            self.addNodetoNode(tuple)
 
-    def addAfterToNode(self, node, after):
-        if node in self.nodes:
-            self.nodes[node].addAfter(after)
+        # self.printNodeNames()
 
-    def checkAfterIsAnyNodesAfter(self, after):
+    def defineNode(self, name: str):
+        if self.getNode(name) == None:
+            self.nodes.append(Node(name))
+
+    def printNodeNames(self):
+        # print("Nodes size: "+ str(len(self.nodes)))
         for node in self.nodes:
-            afters = self.nodes[node].getAfters()
-            if after in afters:
-                print(after, end= '  ')
-                return True
+            print(node.name)
+
+    def getNode(self, name: str):
+        for node in self.nodes:
+            if node.name == name:
+                # print("Node was found!  " + name)
+                return node
+        # print("Node wasnt found! " + name)
+
+    def isNodeAfterNode(self, node1_name: str, node2_name: str):
+        # node1,node2 = self.getNode(node1_name),self.getNode(node2_name)
+        startNode = self.getNode(node1_name)
+        targetNode = self.getNode(node2_name)
+
+        cur_directed_nodes = startNode.getDirectedNodes()
+
+        if targetNode in cur_directed_nodes:
+            return True
+
+        if cur_directed_nodes == None:
             return False
 
-    def printNodes(self):
-        for key in self.nodes:
-            print(key, end=' -> ')
-            self.nodes[key].printAfters()
-            print()
+        return any([self.isNodeAfterNode(node.name, node2_name) for node in cur_directed_nodes])
+
+    def checkIfUpdateValid(self, order:list):
+        currentRoaster = []
+        for curLetter in order:
+            # print("asd")
+            # node = self.getNode(curLetter)
+            for currentRoasterLetter in currentRoaster:
+                # print("asd")
+                if self.isNodeAfterNode(curLetter, currentRoasterLetter):
+                    # print("dsa")
+                    return False
+            currentRoaster.append(curLetter)
+        return True
+
+    def addNodetoNode(self, tuple: tuple):
+        self.getNode(tuple[0]).addNode(self.getNode(tuple[1]))
 
 
-def findWrongOrders(graph):
-    """
-    for node, after in ordering_rules:  # Turn ordering list in to topological sort
-        graph.addNode(node)
-        graph.addAfterToNode(node, after)
-    """
+if __name__ == "__main__":
+    graph = Graph(ordering_rules)
 
-    correctOrders = []
+    sum = 0
     for update in updates:
-        newGraph = Graph()
-        visited = []
-        correct_update = True
-        for symbol in update:
-            visited.append(symbol)
-            correct_update = not newGraph.checkAfterIsAnyNodesAfter(symbol)
-            newGraph.addNode(symbol)
-        if correct_update:
-            correctOrders.append(update)
-        # print(visited)
 
-    print(correctOrders)
+        if graph.checkIfUpdateValid(update):
+            print(update)
+            sum += int(update[int(len(update)/2)]) # Lol great line
 
-
-graph = Graph()
-findWrongOrders(graph)
+    print(int(sum))
