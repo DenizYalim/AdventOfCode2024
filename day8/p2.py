8125095b45cdb78c2e072f7c450b3950ba9d2ced
DenizYalim <116675869+DenizYalim@users.noreply.github.com>
2024-12-11 20:44:48 +0300

    removed debug statements from p1

---
+++ day8/p2.py
@@ -0,0 +1,48 @@
+with open("input", "r") as file:
+    freq_map = file.read()
+    freq_map = freq_map.split('\n')
+
+
+dictAntenna = {}  # Takes char, holds vector for all positions
+dictAntiNode = {}  # Takes x,y tuple returns boolean value of an antiNode is there or not
+
+"""
+if 1 in dictAntiNode:
+    dictAntiNode[1] = [dictAntiNode[1], " hi"]
+else:
+    dictAntiNode[1] = "hi"
+dictAntiNode[1] = [dictAntiNode[1], " hey"]
+print(dictAntiNode[1])
+"""
+
+for i, row in enumerate(freq_map):  # Taking note of the positions of every antenna
+    for j, val in enumerate(row):
+        if val != '.':
+
+            if val in dictAntenna:
+                dictAntenna[val].append((i, j))
+            else:
+                dictAntenna[val] = [(i, j)]
+print(dictAntenna)
+
+
+for key in dictAntenna:
+    for tuple1 in dictAntenna[key]:
+        for tuple2 in dictAntenna[key]:  # We go over repeated tuples of tuples with this. ex: (1,1)x(8,8) and (8,8)x(1,1)
+            if tuple1 != tuple2:
+                dis_x = tuple1[0] - tuple2[0]
+                dis_y = tuple1[1] - tuple2[1]
+                # print(str(tuple1) +" " +  str(tuple2) + " =  " + str(tuple((dis_x, dis_y))))
+
+                if 0 <= tuple1[0] - dis_x < len(freq_map) and 0 <= tuple1[1] - dis_y < len(freq_map):
+                    if freq_map[tuple1[0] - dis_x][tuple1[1] - dis_y] != key:
+                        dictAntiNode[(tuple1[0] - dis_x,tuple1[1] - dis_y)] = True
+
+                if 0 <= tuple2[0] - dis_x < len(freq_map) and 0 <= tuple2[1] - dis_y < len(freq_map):
+                    if freq_map[tuple2[0] - dis_x][ tuple2[1] - dis_y] != key:
+                        dictAntiNode[(tuple2[0] - dis_x, tuple2[1] - dis_y)] = True
+
+                pass
+print(len(dictAntiNode))
+print(dictAntiNode.keys())
+
