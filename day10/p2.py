fa879cb7762efee09bafeb0bcafad5e1015107ea
DenizYalim <116675869+DenizYalim@users.noreply.github.com>
2024-12-13 02:49:48 +0300

    Added d10 p2

--- day10/p2.py
+++ day10/p2.py
@@ -5,9 +5,9 @@ with open('input', 'r') as file:
 directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 
-
-def after(cur, row, col, nineList):
-    # print(f"cur: {cur}, row: {row}, col: {col}")
+count = 0
+def after(cur, row, col):
+    global count
     if 0 <= row < len(topo_map) and 0 <= col < len(topo_map[0]):
         if cur == int(topo_map[row][col]) - 1:
             cur += 1
@@ -17,22 +17,22 @@ def after(cur, row, col, nineList):
         return
 
     if cur == 9:
-        if (row, col) not in nineList:
-            nineList.append((row, col))
+        count += 1
 
-    [after(cur, row+ dx, col+ dy, nineList) for dx,dy in directions]
+    [after(cur, row+ dx, col+ dy) for dx,dy in directions]
 
 
 def after0(cur, row, col):
     nineList = []
-    after(cur, row, col, nineList)
+    after(cur, row, col)
     return len(nineList)
 
 
-count = 0
+
 for rowR, row in enumerate(topo_map):
     for columnC, val in enumerate(row):
         if val == '0':
-            count += after0(-1, rowR, columnC)
+            # print("asd")
+            after0(-1, rowR, columnC)
 
 print(count)
