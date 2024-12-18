dff264d7bcbfc89f37f16cc1d5b556a424c61a7a
DenizYalim <116675869+DenizYalim@users.noreply.github.com>
2024-12-18 04:25:23 +0300

    A slow solution for d9 p2

--- day09/p2.py
+++ day09/p2.py
@@ -6,73 +6,27 @@ for i, char in enumerate(longString):
         [longerString.append(".") for j in range(int(char))]
     else:
         [longerString.append(i // 2) for j in range(int(char))]
+# print(longerString)
 
-print(longerString)
+numbers = sorted(set(longerString) - {'.'}, reverse=True)
 
-right = len(longerString) - 1
-
-while right > 0:
-    left = longerString.index('.')
-    starting_left = left
-    starting_right = right
-    target_number = longerString[right]
-
-    valid = True
-    while left <= right:
-        print("sd")
-
-        valid = True
-        while longerString[right] == target_number:
-            print("sds")
-            right-=1
-            left+=1
-
-            if longerString[left] != '.':
-                valid = False
-                break
-        left += 1
-
-        if valid: # valid, lists can change
-
-            longerString[starting_left:left], longerString[right+1 : starting_right+1] = longerString[right+1: starting_right+1], longerString[starting_left:left]
-        else:
-            right = starting_right
-
-    if valid:
-        right-=1
-
-    while longerString[right] == '.':
-        print("burada olamaz")
-        right-=1
-
-print(longerString)
-"""
-while(right > 0):
-    # print(f"{left}: left:{longerString[left]}, {right}:right: {longerString[right]}, {str(longerString)}")
-    left = longerString.index('.')
-    temp_right = right
-    current_number = longerString[right]
-    invalid = False
-    while left < right and not invalid:
-        temp_left = left
-        invalid = False
-
-        while longerString[right] == current_number:
-            right-=1
-            left+=1
-            if longerString[left] != '.':
-                invalid = True
-
-        if invalid:
-            left = temp_left
-        else:
-            print(f"Valid {longerString[temp_right]}")
-            longerString[temp_left:left], longerString[right + 1 :temp_right+1] = longerString[right + 1:temp_right+1], longerString[temp_left:left]
+for i, number in enumerate(numbers):
+    print(f"{i}: out of {len(numbers)}")
+    countOfNumber = longerString.count(number)
+    for i in range(len(longerString) - countOfNumber + 1):
+        # print("b")
+        if i > longerString.index(number):
             break
-        left+=1
-          while(longerString[right] == '.'):
-        right-=1
-    while(longerString[left] != '.'):
-        left += 1
+        if all(longerString[i+j] == '.' for j in range(countOfNumber)):
+            for _ in range(countOfNumber):
+                # print("c")
+                longerString[longerString.index(number)] = '.'
+            # longerString = longerString[:len(longerString) - countOfNumber] # will cut last digits THIS WONT WORK
+            for j in range(countOfNumber):
+                # print("d")
+                longerString[i+j] = number
+            break
+print(longerString)
+a = sum([i*c for i,c in enumerate(longerString) if c != '.'])
 
-"""
+print(a)
